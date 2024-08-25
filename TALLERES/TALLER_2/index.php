085d79be07be4dad61506bf6400108685b9755fc
BreakMora <edgar07mora17@gmail.com>
2024-08-25 18:37:39 -0500

    Definidas variables de diferentes tipos

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,3 +1,11 @@
 <?php
-    
+    $nombre = "Edgar";
+    $edad = 25;
+    $altura = 1.75;
+    $esEstudiante = true;
+
+    echo "Nombre: $nombre <br>";
+    echo "Edad: $edad <br>";
+    echo "Altura: $altura <br>";
+    echo "¿Es estudiante? " . ($esEstudiante ? "Si" : "No");
 ?>
