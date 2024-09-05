173f6bab20e4305f384e779b59a3891965808c69
Edgar Mora <65365997+BreakMora@users.noreply.github.com>
2024-09-05 18:48:09 -0500

    Agregando los archivos caso 1, parcial 1

---
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -0,0 +1,24 @@
+<?php
+    function contar_palabras($texto){
+        $num_letras = strlen($texto);
+        return $num_letras;
+    }
+
+    function contar_vocales($texto){
+        $vocales = ['a','e','i','o','u'];
+        $contador = 0;
+        $caracteres = str_split($texto);
+        foreach ($caracteres as $caracter) {
+            if (in_array($caracter, $vocales)) {
+                $contador++;
+            }
+        }
+        return $contador;
+    }
+
+    function invertir_palabras($texto){
+        $invertir = strrev($texto);
+        return $invertir;
+    }
+
+?>
